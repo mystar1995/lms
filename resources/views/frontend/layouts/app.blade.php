d94774e3231ede7e9f5964cffe6a037480806401
mystar1995 <jws19950122@outlook,com>
2020-10-01 16:26:26 +0800

    ud

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -6,14 +6,14 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="License2Code">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>License2Code</title>
 
         <!-- Meta -->
         <meta name="description" content="License2Code">
-        <meta name="author" content="@yield('meta_author', 'Viral Solani')">
-        <meta name="keywords" content="@yield('meta_keywords', 'Laravel AdminPanel')">
+        <meta name="author" content="License2Code">
+        <meta name="keywords" content="License2Code">
         <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-js/1.4.0/css/fabric.min.css" />
         <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-js/1.4.0/css/fabric.components.min.css" />
         @yield('meta')
